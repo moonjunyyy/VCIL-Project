5d4644b9a7489cbeb2216f866b8e960f752021ac
moonjunyyy <moonjunyyy@naver.com>
2022-12-09 19:13:24 +0900

    first_commit

--- dataset/cifar100.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# download and unzip dataset
-wget https://github.com/hwany-j/cifar100_png/raw/main/cifar100_png.tar
-
-tar -xvf cifar100_png.tar
-mv cifar100_png cifar100
-echo done
--- models/__init__.py
+++
@@ -1,1 +0,0 @@
-# The codes in this directory were from https://github.com/drimpossible/GDumb/tree/master/src/models
