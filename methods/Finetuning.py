cf1fb6c1eea206c60c38ba9385f0e27ecfdde7cd
moonjunyyy <moonjunyyy@naver.com>
2022-12-30 17:51:54 +0900

    bugfix and git ignore

---
+++ methods/Finetuning.py
@@ -0,0 +1,309 @@
+# When we make a new one, we should inherit the Finetune class.
+import logging
+import copy
+import time
+import datetime
+
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch import optim
+from torchvision import transforms
+from utils.data_loader import ImageDataset, StreamDataset, MemoryDataset, cutmix_data, get_statistics
+from utils.train_utils import select_model, select_optimizer, select_scheduler
+
+logger = logging.getLogger()
+writer = SummaryWriter("tensorboard")
+
+
+def cycle(iterable):
+    # iterate with shuffling
+    while True:
+        for i in iterable:
+            yield i
+
+
+class Finetuning:
+    def __init__(
+            self, criterion, device, train_transform, test_transform, n_classes, **kwargs
+    ):
+        self.num_learned_class = 0
+        self.num_learning_class = 1
+        self.n_classes = n_classes
+        self.exposed_classes = []
+        self.seen = 0
+        self.topk = kwargs["topk"]
+
+        self.device = device
+        self.dataset = kwargs["dataset"]
+        self.model_name = kwargs["model_name"]
+        self.opt_name = kwargs["opt_name"]
+        self.sched_name = kwargs["sched_name"]
+        if self.sched_name == "default":
+            self.sched_name = 'exp_reset'
+        self.lr = kwargs["lr"]
+
+        self.train_transform = train_transform
+        self.cutmix = "cutmix" in kwargs["transforms"]
+        self.test_transform = test_transform
+
+        self.memory_size = kwargs["memory_size"]
+        self.data_dir = kwargs["data_dir"]
+
+        self.online_iter = kwargs["online_iter"]
+        self.batch_size = kwargs["batchsize"]
+        self.temp_batchsize = kwargs["temp_batchsize"]
+        if self.temp_batchsize is None:
+            self.temp_batchsize = self.batch_size//2
+        if self.temp_batchsize > self.batch_size:
+            self.temp_batchsize = self.batch_size
+        self.memory_size -= self.temp_batchsize
+
+        self.gpu_transform = kwargs["gpu_transform"]
+        self.use_amp = kwargs["use_amp"]
+        if self.use_amp:
+            self.scaler = torch.cuda.amp.GradScaler()
+
+        self.model = select_model(self.model_name, self.dataset, 1).to(self.device)
+        self.optimizer = select_optimizer(self.opt_name, self.lr, self.model,is_vit=True)
+        if 'imagenet' in self.dataset:
+            self.lr_gamma = 0.99995
+        else:
+            self.lr_gamma = 0.9999
+        self.scheduler = select_scheduler(self.sched_name, self.optimizer, self.lr_gamma)
+
+        self.criterion = criterion.to(self.device)
+        self.memory = MemoryDataset(self.train_transform, cls_list=self.exposed_classes,
+                                    test_transform=self.test_transform)
+        self.temp_batch = []
+        self.num_updates = 0
+        self.train_count = 0
+        self.batch_size = kwargs["batchsize"]
+
+        self.start_time = time.time()
+        num_samples = {'cifar10': 50000, 'cifar100': 50000, 'tinyimagenet': 100000, 'imagenet': 1281167}
+        self.total_samples = num_samples[self.dataset]
+
+    def online_step(self, sample, sample_num, n_worker):
+        image, label = sample
+        for l in label:
+            if l.item() not in self.exposed_classes:
+                self.add_new_class(l.item())
+
+        self.num_updates += self.online_iter * self.batch_size
+        # if len(self.temp_batch) == self.temp_batchsize:
+        train_loss, train_acc = self.online_train([image, label], self.batch_size * 2, n_worker,
+                                                    iterations=int(self.num_updates), stream_batch_size=self.batch_size)
+        self.report_training(sample_num, train_loss, train_acc)
+        for stored_sample, stored_label in zip(image, label):
+            self.update_memory((stored_sample, stored_label))
+        self.temp_batch = []
+        self.num_updates -= int(self.num_updates)
+
+    def add_new_class(self, class_name):
+        self.exposed_classes.append(class_name)
+        self.num_learned_class = len(self.exposed_classes)
+        prev_weight = copy.deepcopy(self.model.head.weight.data)
+        self.model.head = nn.Linear(self.model.head.in_features, self.num_learned_class).to(self.device)
+
+        with torch.no_grad():
+            if self.num_learned_class > 1:
+                self.model.head.weight[:self.num_learned_class - 1] = prev_weight
+        for param in self.optimizer.param_groups[1]['params']:
+            if param in self.optimizer.state.keys():
+                del self.optimizer.state[param]
+        del self.optimizer.param_groups[1]
+        self.optimizer.add_param_group({'params': self.model.head.parameters()})
+        self.memory.add_new_class(cls_list=self.exposed_classes)
+        if 'reset' in self.sched_name:
+            self.update_schedule(reset=True)
+
+    def online_train(self, sample, batch_size, n_worker, iterations=1, stream_batch_size=1):
+        total_loss, correct, num_data = 0.0, 0.0, 0.0
+        # if stream_batch_size > 0:
+        #     sample_dataset = StreamDataset(sample, transform=self.train_transform, cls_list=self.exposed_classes)
+
+        # if len(self.memory) > 0 and batch_size - stream_batch_size > 0:
+        #     memory_batch_size = min(len(self.memory), batch_size - stream_batch_size)
+
+        for i in range(iterations):
+            self.model.train()
+            # x = []
+            # y = []
+            x, y = sample
+            x = torch.cat([self.train_transform(transforms.ToPILImage()(img)).unsqueeze(0) for img in x])
+            y = torch.cat([torch.tensor([self.exposed_classes.index(label)]) for label in y])
+            # if stream_batch_size > 0:
+            #     # sample = sample_dataset.get_data()
+            #     x.append(sample['image'])
+            #     y.append(sample['label'])
+            if len(self.memory) > 0:
+                memory_data = self.memory.get_batch(y.size(0))
+                x = torch.cat([x, memory_data['image']])
+                y = torch.cat([y, memory_data['label']])
+                # x.append(memory_data['image'])
+                # y.append(memory_data['label'])
+            # x = torch.cat([x])
+            # y = torch.cat([y])
+
+            x = x.to(self.device)
+            y = y.to(self.device)
+            # print("[Target shape]",y.shape)
+
+            self.optimizer.zero_grad()
+
+            logit, loss = self.model_forward(x,y)
+
+            _, preds = logit.topk(self.topk, 1, True, True)
+
+            if self.use_amp:
+                self.scaler.scale(loss).backward()
+                self.scaler.step(self.optimizer)
+                self.scaler.update()
+            else:
+                loss.backward()
+                self.optimizer.step()
+
+            self.update_schedule()
+
+            total_loss += loss.item()
+            correct += torch.sum(preds == y.unsqueeze(1)).item()
+            num_data += y.size(0)
+
+        return total_loss / iterations, correct / num_data
+
+    def model_forward(self, x, y):
+        do_cutmix = self.cutmix and np.random.rand(1) < 0.5
+        if do_cutmix:
+            # print("[do_Cutmix]")
+            x, labels_a, labels_b, lam = cutmix_data(x=x, y=y, alpha=1.0)
+            
+            if self.use_amp:
+                with torch.cuda.amp.autocast():
+                    logit = self.model(transforms.Resize((224,224))(x))['logits']
+                    loss = lam * self.criterion(logit, labels_a) + (1 - lam) * self.criterion(logit, labels_b)
+            else:
+                logit = self.model(transforms.Resize((224,224))(x))['logits']
+                loss = lam * self.criterion(logit, labels_a) + (1 - lam) * self.criterion(logit, labels_b)
+        else:
+            # print("[do_Not_Cutmix]")
+            if self.use_amp:
+                # x =
+                with torch.cuda.amp.autocast():
+                    logit = self.model(transforms.Resize((224,224))(x))['logits']
+                    loss = self.criterion(logit, y)
+            else:
+                logit = self.model(transforms.Resize((224,224))(x))['logits']
+                loss = self.criterion(logit, y)
+        return logit, loss
+
+    def report_training(self, sample_num, train_loss, train_acc):
+        writer.add_scalar(f"train/loss", train_loss, sample_num)
+        writer.add_scalar(f"train/acc", train_acc, sample_num)
+        logger.info(
+            f"Train | Sample # {sample_num} | train_loss {train_loss:.4f} | train_acc {train_acc:.4f} | "
+            f"lr {self.optimizer.param_groups[0]['lr']:.6f} | "
+            f"running_time {datetime.timedelta(seconds=int(time.time() - self.start_time))} | "
+            f"ETA {datetime.timedelta(seconds=int((time.time() - self.start_time) * (self.total_samples-sample_num) / sample_num))}"
+        )
+
+    def report_test(self, sample_num, avg_loss, avg_acc):
+        writer.add_scalar(f"test/loss", avg_loss, sample_num)
+        writer.add_scalar(f"test/acc", avg_acc, sample_num)
+        logger.info(
+            f"Test | Sample # {sample_num} | test_loss {avg_loss:.4f} | test_acc {avg_acc:.4f} | "
+        )
+
+    def update_memory(self, sample):
+        self.reservoir_memory(sample)
+
+    def update_schedule(self, reset=False):
+        if reset:
+            self.scheduler = select_scheduler(self.sched_name, self.optimizer, self.lr_gamma)
+            for param_group in self.optimizer.param_groups:
+                param_group["lr"] = self.lr
+        else:
+            self.scheduler.step()
+            
+    def online_evaluate(self, test_loader, sample_num):
+        eval_dict = self.evaluation(test_loader, self.criterion)
+        self.report_test(sample_num, eval_dict["avg_loss"], eval_dict["avg_acc"])
+        return eval_dict
+
+    def online_before_task(self, cur_iter):
+        # Task-Free
+        pass
+
+    def online_after_task(self, cur_iter):
+        # Task-Free
+        pass
+
+    def reservoir_memory(self, sample):
+        self.seen += 1
+        if len(self.memory.images) >= self.memory_size:
+            j = np.random.randint(0, self.seen)
+            if j < self.memory_size:
+                self.memory.replace_sample(sample, j)
+        else:
+            self.memory.replace_sample(sample)
+
+    def reset_opt(self):
+        self.optimizer = select_optimizer(self.opt_name, self.lr, self.model)
+        self.scheduler = select_scheduler(self.sched_name, self.optimizer, self.lr_gamma)
+
+    def evaluation(self, test_loader, criterion):
+        total_correct, total_num_data, total_loss = 0.0, 0.0, 0.0
+        correct_l = torch.zeros(self.n_classes)
+        num_data_l = torch.zeros(self.n_classes)
+        label = []
+
+        self.model.eval()
+        with torch.no_grad():
+            for i, data in enumerate(test_loader):
+                x, y = data
+                for j in range(len(y)):
+                    y[j] = self.exposed_classes.index(y[j].item())
+                x = x.to(self.device)
+                y = y.to(self.device)
+                # logit = self.model(x)['logits']
+                logit = self.model(transforms.Resize((224,224))(x))['logits']
+
+                loss = criterion(logit, y)
+                pred = torch.argmax(logit, dim=-1)
+                _, preds = logit.topk(self.topk, 1, True, True)
+
+                total_correct += torch.sum(preds == y.unsqueeze(1)).item()
+                total_num_data += y.size(0)
+
+                xlabel_cnt, correct_xlabel_cnt = self._interpret_pred(y, pred)
+                correct_l += correct_xlabel_cnt.detach().cpu()
+                num_data_l += xlabel_cnt.detach().cpu()
+
+                total_loss += loss.item()
+                label += y.tolist()
+
+        avg_acc = total_correct / total_num_data
+        avg_loss = total_loss / len(test_loader)
+        cls_acc = (correct_l / (num_data_l + 1e-5)).numpy().tolist()
+        ret = {"avg_loss": avg_loss, "avg_acc": avg_acc, "cls_acc": cls_acc}
+
+        return ret
+
+    def _interpret_pred(self, y, pred):
+        # xlable is batch
+        ret_num_data = torch.zeros(self.n_classes)
+        ret_corrects = torch.zeros(self.n_classes)
+
+        xlabel_cls, xlabel_cnt = y.unique(return_counts=True)
+        for cls_idx, cnt in zip(xlabel_cls, xlabel_cnt):
+            ret_num_data[cls_idx] = cnt
+
+        correct_xlabel = y.masked_select(y == pred)
+        correct_cls, correct_cnt = correct_xlabel.unique(return_counts=True)
+        for cls_idx, cnt in zip(correct_cls, correct_cnt):
+            ret_corrects[cls_idx] = cnt
+
+        return ret_num_data, ret_corrects
---
+++ utils/train_utils.py
@@ -0,0 +1,222 @@
+import torch_optimizer
+from easydict import EasyDict as edict
+from torch import optim
+
+from models import mnist, cifar, imagenet
+from timm.models.registry import register_model
+from timm.models.vision_transformer import _cfg, _create_vision_transformer, default_cfgs
+from timm.models import create_model
+from models.cifar_vit import _create_vision_transformer
+
+@register_model
+def vit_base_patch16_224(pretrained=False, **kwargs):
+    """ ViT-Base model (ViT-B/32) from original paper (https://arxiv.org/abs/2010.11929).
+    ImageNet-21k weights @ 224x224, source https://github.com/google-research/vision_transformer.
+    NOTE: this model has valid 21k classifier head and no representation (pre-logits) layer
+    """
+    model_kwargs = dict(
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, **kwargs)
+    model = _create_vision_transformer('vit_base_patch16_224', pretrained=pretrained, **model_kwargs)
+    return model
+
+
+def cycle(iterable):
+    # iterate with shuffling
+    while True:
+        for i in iterable:
+            yield i
+
+
+def select_optimizer(opt_name, lr, model,is_vit=False):
+    if is_vit:
+        if opt_name == "adam":
+            params = [param for name, param in model.named_parameters() if 'head' not in name]
+            opt = optim.Adam(params, lr=lr, weight_decay=0)
+            opt.add_param_group({'params': model.head.parameters()})
+        elif opt_name == "radam":
+            params = [param for name, param in model.named_parameters() if 'head' not in name]
+            opt = torch_optimizer.RAdam(params, lr=lr, weight_decay=0.00001)
+            opt.add_param_group({'params': model.head.parameters()})
+        elif opt_name == "sgd":
+            params = [param for name, param in model.named_parameters() if 'head' not in name]
+            opt = optim.SGD(
+                params, lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4
+            )
+            opt.add_param_group({'params': model.head.parameters()})
+        else:
+            raise NotImplementedError("Please select the opt_name [adam, sgd] -- is_vit True!")
+    else:    
+        if opt_name == "adam":
+            # print("opt_name: adam")
+            params = [param for name, param in model.named_parameters() if 'fc' not in name]
+            opt = optim.Adam(params, lr=lr, weight_decay=0)
+            opt.add_param_group({'params': model.fc.parameters()})
+        elif opt_name == "radam":
+            params = [param for name, param in model.named_parameters() if 'fc' not in name]
+            opt = torch_optimizer.RAdam(params, lr=lr, weight_decay=0.00001)
+            opt.add_param_group({'params': model.fc.parameters()})
+        elif opt_name == "sgd":
+            params = [param for name, param in model.named_parameters() if 'fc' not in name]
+            opt = optim.SGD(
+                params, lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4
+            )
+            opt.add_param_group({'params': model.fc.parameters()})
+        else:
+            raise NotImplementedError("Please select the opt_name [adam, sgd]")
+    return opt
+
+def select_optimizer_with_extern_params(opt_name, lr, model,is_vit=False,extern_param=None):
+    #todo model--> pretrained and freeze, train external parameters and head layer
+    if is_vit:
+        if opt_name == "adam":
+            params = [param for name, param in extern_param.named_parameters() if 'head' not in name]
+            opt = optim.Adam(params, lr=lr, weight_decay=0)
+            opt.add_param_group({'params': model.head.parameters()})
+        elif opt_name == "radam":
+            params = [param for name, param in extern_param.named_parameters() if 'head' not in name]
+            opt = torch_optimizer.RAdam(params, lr=lr, weight_decay=0.00001)
+            opt.add_param_group({'params': model.head.parameters()})
+        elif opt_name == "sgd":
+            params = [param for name, param in extern_param.named_parameters() if 'head' not in name]
+            opt = optim.SGD(
+                params, lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4
+            )
+            opt.add_param_group({'params': model.head.parameters()})
+        else:
+            raise NotImplementedError("Please select the opt_name [adam, sgd] -- is_vit True!")
+    else:    
+        if opt_name == "adam":
+            params = [param for name, param in extern_param.named_parameters() if 'fc' not in name]
+            opt = optim.Adam(params, lr=lr, weight_decay=0)
+            opt.add_param_group({'params': model.fc.parameters()})
+        elif opt_name == "radam":
+            params = [param for name, param in extern_param.named_parameters() if 'fc' not in name]
+            opt = torch_optimizer.RAdam(params, lr=lr, weight_decay=0.00001)
+            opt.add_param_group({'params': model.fc.parameters()})
+        elif opt_name == "sgd":
+            params = [param for name, param in extern_param.named_parameters() if 'fc' not in name]
+            opt = optim.SGD(
+                params, lr=lr, momentum=0.9, nesterov=True, weight_decay=1e-4
+            )
+            opt.add_param_group({'params': model.fc.parameters()})
+        else:
+            raise NotImplementedError("Please select the opt_name [adam, sgd]")
+    return opt
+
+def select_scheduler(sched_name, opt, hparam=None):
+    if "exp" in sched_name:
+        scheduler = optim.lr_scheduler.ExponentialLR(opt, gamma=hparam)
+    elif sched_name == "cos":
+        scheduler = optim.lr_scheduler.CosineAnnealingWarmRestarts(opt, T_0=1, T_mult=2)
+    elif sched_name == "anneal":
+        scheduler = optim.lr_scheduler.ExponentialLR(opt, 1 / 1.1, last_epoch=-1)
+    elif sched_name == "multistep":
+        scheduler = optim.lr_scheduler.MultiStepLR(opt, milestones=[30, 60, 80, 90], gamma=0.1)
+    elif sched_name == "const":
+        scheduler = optim.lr_scheduler.LambdaLR(opt, lambda iter: 1)
+    else:
+        scheduler = optim.lr_scheduler.LambdaLR(opt, lambda iter: 1)
+    return scheduler
+
+
+# def select_model(model_name, dataset, num_classes=None):
+#     opt = edict(
+#         {
+#             "depth": 18,
+#             "num_classes": num_classes,
+#             "in_channels": 3,
+#             "bn": True,
+#             "normtype": "BatchNorm",
+#             "activetype": "ReLU",
+#             "pooltype": "MaxPool2d",
+#             "preact": False,
+#             "affine_bn": True,
+#             "bn_eps": 1e-6,
+#             "compression": 0.5,
+#         }
+#     )
+
+#     if "mnist" in dataset:
+#         model_class = getattr(mnist, "MLP")
+#     elif "cifar" in dataset:
+#         model_class = getattr(cifar, "ResNet")
+#     elif "imagenet" in dataset:
+#         model_class = getattr(imagenet, "ResNet")
+#     else:
+#         raise NotImplementedError(
+#             "Please select the appropriate datasets (mnist, cifar10, cifar100, imagenet)"
+#         )
+#     if model_name == "resnet18":
+#         opt["depth"] = 18
+#     elif model_name == "resnet32":
+#         opt["depth"] = 32
+#     elif model_name == "resnet34":
+#         opt["depth"] = 34
+#     elif model_name == "mlp400":
+#         opt["width"] = 400
+#     else:
+#         raise NotImplementedError(
+#             "Please choose the model name in [resnet18, resnet32, resnet34]"
+#         )
+
+#     model = model_class(opt)
+
+#     return model
+
+
+def select_model(model_name, dataset, num_classes=None):
+    opt = edict(
+        {
+            "depth": 18,
+            "num_classes": num_classes,
+            "in_channels": 3,
+            "bn": True,
+            "normtype": "BatchNorm",
+            "activetype": "ReLU",
+            "pooltype": "MaxPool2d",
+            "preact": False,
+            "affine_bn": True,
+            "bn_eps": 1e-6,
+            "compression": 0.5,
+        }
+    )
+
+#! cifar and imageNet --> ViT model 추가!!
+    if "mnist" in dataset:
+        model_class = getattr(mnist, "MLP")
+    elif "cifar" in dataset:
+        model_class = getattr(cifar, "ResNet")
+    elif "imagenet" in dataset:
+        model_class = getattr(imagenet, "ResNet")
+    elif "vit" in dataset:
+        pass
+    else:
+        raise NotImplementedError(
+            "Please select the appropriate datasets (mnist, cifar10, cifar100, imagenet)"
+        )
+
+    #* vit method(L2p) --> cifar_vit,vision_transformer
+
+    if model_name == "resnet18":
+        opt["depth"] = 18
+    elif model_name == "resnet32":
+        opt["depth"] = 32
+    elif model_name == "resnet34":
+        opt["depth"] = 34
+    elif model_name == "mlp400":
+        opt["width"] = 400
+    elif model_name == "vit":
+        opt["depth"] = 12
+    else:
+        raise NotImplementedError(
+            "Please choose the model name in [resnet18, resnet32, resnet34]"
+        )
+    if model_name == "vit":
+        model = create_model(
+                            "vit_base_patch16_224",pretrained=True,num_classes=num_classes,
+                            drop_rate=0.,drop_path_rate=0.,drop_block_rate=None,)
+    else:
+        model = model_class(opt)
+
+    print("[Selected Model]:", model_name )
+    return model
