e08d7a5203fa69844d41b272a0c0cfdf293b4113
moonjunyyy <moonjunyyy@naver.com>
2023-01-18 19:49:13 +0900

    deleted unused files

--- scripts/finetuning.sh
+++ scripts/finetuning.sh
@@ -85,5 +85,5 @@ do
     --model_name $MODEL_NAME --opt_name $OPT_NAME --sched_name $SCHED_NAME \
     --lr $LR --batchsize $BATCHSIZE --n_worker 4 \
     --memory_size $MEM_SIZE $GPU_TRANSFORM --online_iter $ONLINE_ITER --data_dir /local_datasets/ \
-    --note $NOTE --eval_period $EVAL_PERIOD --memory_epoch $MEMORY_EPOCH $USE_AMP 
+    --note $NOTE --eval_period $EVAL_PERIOD --memory_epoch $MEMORY_EPOCH --n_worker 4
 done
